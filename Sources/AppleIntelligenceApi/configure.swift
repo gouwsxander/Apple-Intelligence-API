fe30dbd72911a612befc1de89d8ba4374209cfac
Xander <gouws.xander@gmail.com>
2025-09-28 16:07:54 -0300

    Adds CORS middleware

--- Sources/AppleIntelligenceApi/configure.swift
+++ Sources/AppleIntelligenceApi/configure.swift
@@ -20,6 +20,13 @@ public func configure(_ app: Application) async throws {
 //    app.migrations.add(CreateTokens())
 //    try app.autoMigrate().wait()
     
+    let corsConfiguration = CORSMiddleware.Configuration(
+            allowedOrigin: .all,
+            allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+            allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith]
+        )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    app.middleware.use(cors, at: .beginning)
     
     // register routes
     try routes(app)
